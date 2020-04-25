7c1052114e9eb66e7a92ef212b3b6f1eb99f04ca
Sutikshan <s.anand.dubey@gmail.com>
2020-04-26 09:01:31 +1000

    rename resources to routes

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { getUser } = require('./resources/user');
-const { getProducts } = require('./resources/products');
-const { getTrolleyTotal } = require('./resources/trolley');
+const { getUser } = require('./routes/user');
+const { getProducts } = require('./routes/products');
+const { getTrolleyTotal } = require('./routes/trolley');
 
 const router = express.Router();
 const jsonParser = bodyParser.json();
